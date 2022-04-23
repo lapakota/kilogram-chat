7f3221f43537d3a95a3fc4be30cad711b7e45c30
lapakota <stalkerzone955@gmail.com>
2022-04-23 19:41:12 +0500

    refactor

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Getting Started with Create React App
+# Getting Started with Create React Index
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+This project was bootstrapped with [Create React Index](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
 
@@ -26,6 +26,6 @@ See the section about [deployment](https://facebook.github.io/create-react-app/d
 
 ## Learn More
 
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
+You can learn more in the [Create React Index documentation](https://facebook.github.io/create-react-app/docs/getting-started).
 
 To learn React, check out the [React documentation](https://reactjs.org/).
--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,1 +1,58 @@
-export { App as default } from "./App";
+import { useEffect, useState } from "react"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+import styles from "./index.module.css"
+import { Login } from "../Login/login"
+import { Register } from "../Register"
+import { Provider } from "react-redux"
+import { store } from "../../store/store"
+import ChatPage from "../Chat"
+
+export function App() {
+  const [data, setData] = useState({ chats: [] })
+
+  useEffect(() => {
+    const controller = new AbortController()
+
+    fetch("https://kilogram-api.yandex-urfu-2021.ru/query", {
+      signal: controller.signal,
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        query: `{
+          chats {
+            image
+            name
+            messages {
+              createdBy { 
+                image
+                login 
+                name
+              }
+        
+              text
+            }
+          }
+        }`,
+      }),
+    })
+      .then((response) => response.json())
+      .then((json) => setData(json.data))
+
+    return () => controller.abort()
+  }, [])
+
+  return (
+    <Provider store={store}>
+      <div className={styles.app}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/chat" element={<ChatPage />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </Provider>
+  )
+}
