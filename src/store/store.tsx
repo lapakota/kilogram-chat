c73a0afdc6e44702c9d3a8cbd3ebf5e77d2d9cba
Misha <63599737+MichailNonProgramer@users.noreply.github.com>
2022-05-01 19:30:58 +0500

    Delete message slice

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -1,6 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit"
 import userReducer from "./slices/userSlice"
-import messageReducer from "./slices/messageSlice"
 import chatReducer from "./slices/chatSlice"
 import chatsReducer from "./slices/chatsSlice"
 import usersReducer from "./slices/usersSlice"
@@ -8,7 +7,6 @@ import usersReducer from "./slices/usersSlice"
 export const store = configureStore({
   reducer: {
     user: userReducer,
-    message: messageReducer,
     chat: chatReducer,
     chats: chatsReducer,
     users: usersReducer,
